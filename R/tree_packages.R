852e38aa4831cec6577a85cfc4d674d6f422991c
SoniaFlores1 <floressonia722@gmail.com>
2022-03-31 11:20:07 -0600

    more alignment and tree building

--- R/tree_packages.R
+++ R/tree_packages.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(BiocManager)
 library(ape)
 library(phangorn)
-#Now download the ggtree, msa, andBiostrings package from Bioconductor.
+#Now download the ggtree, msa, and Biostrings package from Bioconductor.
 BiocManager::install("ggtree")
 BiocManager::install("Biostrings")
 BiocManager::install("msa")
@@ -17,4 +17,29 @@ library(msa)
 read.FASTA(file ="./Data/Raw/archosauria_18s_rrna.fasta")
 
 Biostrings::readDNAStringSet(filepath = "./Data/Raw/archosauria_18s_rrna.fasta")
+
+#Reading in sequences as a DNAStringSet
+filepath<- "./Data/Raw/archosauria_18s_rrna.fasta"
+mySeqs<- readDNAStringSet(filepath)
+
+
+#sequence alignment (ClustalW and Muscle)
+alignment <- msa::msaClustalW(inputSeqs =mySeqs, cluster = "nj",type = "dna")
+alignmentM<- msa::msaMuscle(inputSeqs = mySeqs, cluster = "neighborjoining", type = "dna")
+
+#Conservation scores
+data("BLOSUM62")
+ClustalWScore<-msa::msaConservationScore(alignment, BLOSUM62)
+
+
+align_phyDat<-msa::msaConvert(x=alignment, type = "phangorn::phyDat")
+
+#In Phangorn now, making a distance matrix (Maximum Likelihood)
+phydist <- dist.ml(align_phyDat)
+
+#applying neighborjoining 
+nj <- phangorn::NJ(phydist)
+
+#plotting a tree
+plot(nj, main="nj")
 
